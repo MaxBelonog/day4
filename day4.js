6d86d16afaf8e2cc566866400f448a8b5d75e6ee
MaxBelonog <32751103+MaxBelonog@users.noreply.github.com>
2017-11-30 16:22:49 -0800

    Add files via upload

--- day4.js
+++ day4.js
@@ -1,24 +1,7 @@
-//New Car Object
-function createNewCar(newModel, newYear, newMake, newColor) {
-  return {
-  model: newModel,
-  year: newYear,
-  make: newMake,
-  color: newColor,
-  getData: function() {
-    return this.model+" "+this. year+" "+this.make+" "+this.color
-    }
-  };
-}
-
-var car = createNewCar("Liberty", 2008, "Jeep", "Black");
-
-
-
-//
+// Car Object Challenge
 
 var newCar = function createNewCar(newModel, newYear, newMake, newColor) {
-  var speed = 0;
+  var speed = 0
   return {
     model: newModel,
     year: newYear,
@@ -43,9 +26,37 @@ var newCar = function createNewCar(newModel, newYear, newMake, newColor) {
         console.log(speed);
       }
     },
-
+    slowCar: function(){
+      while (speed > 0){
+        this.brake();
+        speed = this.getSpeed();
+        console.log(speed);
+      }
+    }
   };
 }
 
-var liberty = newCar("Liberty", 2008, "Jeep", "Black");
+var liberty = newCar("Liberty", 2008, "Jeep", "Black")
 var x = newCar("Truck", 2005, "truck", "purple");
+
+
+
+//Object Oriented Password Checker
+var askForUsername = prompt("What is your Username?");
+var askForPassword = prompt("What is your Password?");
+var newLogin = function createNewLogin (newUsername, newPassword){
+  return{
+    Username: newUsername,
+    Password: newPassword,
+  };
+}
+
+var jake = newLogin(askForUsername, askForPassword);
+
+function userChecker(user){
+  if ((typeof askForUsername === "string") && (typeof askForPassword=== "string")) {
+    alert("Thanks for signing up!");
+  }else{
+    alert("Letters only. Try Again");
+  }
+}
